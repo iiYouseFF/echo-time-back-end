a4e2aa3498ad33d1ced2eb3e02b22a356c1a2c92
Yousef Amr <amryosef360@gmail.com>
2026-04-07 19:57:21 +0200

    feat: implement ReviewRepository with create and average rating methods

--- src/modules/review/review.repository.js
+++ src/modules/review/review.repository.js
@@ -1,4 +1,4 @@
-import { supabase } from "../../config/supabseClient.js";
+import { supabase } from "../../config/supabaseClient.js";
 import { BaseRepository } from "../BaseRepository.js";
 
 export class ReviewRepository extends BaseRepository {
